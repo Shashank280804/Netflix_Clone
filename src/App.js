173957428198b47ab2a77dd8e6cd27dc92624be8
shashank <116015929+Shashank280804@users.noreply.github.com>
2024-08-21 23:57:14 +0530

    Added the comments

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import { Provider } from "react-redux";
-import Body from "./components/Body";
-import appStore from "./utils/appStore";
+import { Provider } from "react-redux"; // Importing the Provider component from react-redux to connect the Redux store to the React application
+import Body from "./components/Body"; // Importing the Body component, which likely represents the main content of the app
+import appStore from "./utils/appStore"; // Importing the Redux store from the appStore file
 
 function App() {
   return (
+    // Wrapping the app with the Provider component to pass down the Redux store to all nested components
     <Provider store={appStore}>
-      <Body />
+      <Body /> {/* Rendering the Body component, which will now have access to the Redux store */}
     </Provider>
   );
 }
 
-export default App;
+export default App; // Exporting the App component as the default export
